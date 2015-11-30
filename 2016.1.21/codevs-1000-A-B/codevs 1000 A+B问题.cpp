94066236d0aa4011531fca31c402c146e9a41d9e
ArticulateG <ArticulateG@users.noreply.github.com>
2015-11-30 20:27:14 +0800

    Create codevs 1000 A+B问题.cpp

---
+++ 2016.1.21/codevs-1000-A-B/codevs 1000 A+B问题.cpp
@@ -0,0 +1,14 @@
+/*
+    codevs 1000 A+B问题
+    http://codevs.cn/problem/1000/
+    基本输入输出
+*/
+#inlclude <cstdio>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    return 0;
+}
